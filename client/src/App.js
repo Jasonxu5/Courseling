a4cf82f36599f35156e174a805177fb47ab75ca9
Jason Xu <jasonxu515@gmail.com>
2022-11-11 20:59:04 -0800

    Fix extension error

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
-// RUN python server.py then in another terminal npm start 
+// RUN python server.py then in another terminal npm start
 // npm build and load unpacked in chrome extensions to run extension
 // making into extensions doesnt work. how to make app.js rendering to a html?
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
   const [data, setData] = useState([{}])
 
   useEffect(() => {
-    fetch("/members").then(
-        res => res.json()
+    fetch("http://localhost:3000/members").then(
+      res => res.json()
     ).then(
-        data => {
-            setData(data)
-            console.log(data)
-        }
+      data => {
+        setData(data)
+        console.log(data)
+      }
     )
   }, [])
 
   return (
     <div>
 
-      {(typeof data.members ==='undefined') ? (
+      {(typeof data.members === 'undefined') ? (
         <p>Loading...</p>
       ) : (
         data.members.map((member, i) => (
-            <p key ={i}>{member}</p>
-          ))
+          <p key={i}>{member}</p>
+        ))
       )}
 
     </div>
