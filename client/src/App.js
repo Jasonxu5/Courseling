5a8e8c7ce73d073dcdc7395df1b48cdcda6d7137
Jason Xu <jasonxu515@gmail.com>
2022-12-06 00:42:56 -0800

    Add search box

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import "./styles.css"
 
 const apiEndpoint = "http://localhost:5000/"
 
 function App() {
   const [input, setInput] = useState('')
+  const [html, set_html] = useState(null)
+  const [result, set_result] = useState(null)
 
   const handleButtonClicked = async () => {
     let table = document.getElementById('table')
@@ -36,7 +39,6 @@ function App() {
 
       course.textContent = courses[i]
       score.textContent = scores[i]
-      // tr.insertCell().textContent = i
       extTable.append(course)
       extTable.append(score)
       parent.append(extTable)
@@ -45,14 +47,42 @@ function App() {
     console.log(parent)
 
     table.append(parent)
+    displayCourseSearcher()
+  }
+
+  const handleCourseButtonClicked = async () => {
+    let searchQuery = document.getElementById('search-box').value
+
+    let response = await fetch(apiEndpoint + `search?query=${searchQuery}`)
+    let json = await response.json()
+
+    console.log(json)
+
+    set_result(
+      <div>
+        <p>{json.description}</p>
+      </div>
+    )
+  }
+
+  function displayCourseSearcher() {
+    set_html(
+      <div id="search-area">
+        <label>Search for a course:</label>
+        <input id="search-box" type="text" size="30" />
+        <button onClick={() => { handleCourseButtonClicked() }}>Search</button>
+      </div>
+    )
   }
 
   return (
     <div>
       <label>Input an query:</label>
-      <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+      <input type="text" value={input} onChange={e => setInput(e.target.value)} size="30" />
       <button onClick={() => { handleButtonClicked() }}>Submit</button>
       <div id="table"></div>
+      {html}
+      {result}
     </div>
   )
 }
